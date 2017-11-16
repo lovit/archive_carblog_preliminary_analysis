eb4d1a18a227038eac6bd0f7fc2191c1cb96bf57
lovit <soy.lovit@gmail.com>
2017-11-17 05:54:43 +0900

    Change tokenizer from cohesion * droprate to only droprate score

--- make_subwords_slot.py
+++ make_subwords_slot.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 from glob import glob
 
-def parse_subwords(corpus_fnames, model_directory, soynlp_path, min_frequency, subword_max_length, expansion_threshold):    
+def parse_subwords(corpus_fnames, model_directory, soynlp_path, min_frequency, subword_max_length, minimum_droprate_score):
     try:
         sys.path.append(soynlp_path)
         from soynlp.utils import DoublespaceLineCorpus
@@ -19,24 +19,23 @@ def parse_subwords(corpus_fnames, model_directory, soynlp_path, min_frequency, s
         model_fname = '{}/{}_subword_statistics.pkl'.format(model_directory, corpus_index)        
         
         L, df_subword, n_doc = _subword_counting(corpus, subword_max_length)
-        cohesions, droprate_scores, word_scores = _word_scoring(L, min_frequency, expansion_threshold)
-        df_word = _word_extraction(corpus, subword_max_length, word_scores)
-        _save(model_fname, L, df_subword, df_word, cohesions, droprate_scores, word_scores, n_doc)
+        cohesions, droprate_scores = _word_scoring(L, min_frequency)
+        df_word = _word_extraction(corpus, subword_max_length, droprate_scores, minimum_droprate_score)
+        _save(model_fname, L, df_subword, df_word, cohesions, droprate_scores, n_doc)
         
         print('\r  - scanning and computation were done {} / {}, used memory = {} Gb'.format(n_corpus+1, len(corpus_fnames), ' %.3f'%get_process_memory()))        
-        del cohesions, df_subword, df_word, L, word_scores
+        del cohesions, droprate_scores, df_subword, df_word, L
         
     print('  subword parsing was done')
     return True
 
-def _save(model_fname, L, df_subword, df_word, cohesions, droprate_scores, word_scores, n_doc):
+def _save(model_fname, L, df_subword, df_word, cohesions, droprate_scores, n_doc):
     params = {
         'frequency':L,
         'document_frequency_subword':df_subword,
         'document_frequency_word':df_word,
         'cohesion': cohesions,
         'droprate_score': droprate_scores,
-        'word_score': word_scores,
         'num_doc': n_doc
     }
     with open(model_fname, 'wb') as f:
@@ -50,44 +49,40 @@ def _subword_counting(corpus, subword_max_length):
             for e in range(1, min(subword_max_length, len(word))+1):
                 subword = word[:e]
                 L[subword] = L.get(subword, 0) + 1
+                subwords.add(subword)
         for subword in filter(lambda x:len(x) > 1, subwords):
-            df[subword] = df.get(subword, 0) + 1            
+            df[subword] = df.get(subword, 0) + 1
         if n_doc % 1000 == 999:
             print('\r  - scanning ... {} docs'.format(n_doc+1), flush=True, end='')
     return L, df, (n_doc+1)
 
-def _word_scoring(L, min_frequency, expansion_threshold):
-    cohesions, droprate_scores, word_scores = {}, {}, {}
+def _word_scoring(L, min_frequency):
+    cohesions, droprate_scores = {}, {}
     for l, count in sorted(L.items(), key=lambda x:len(x[0])):
         n = len(l)
+        # Cohesion
         if n < 2 or count < min_frequency:
             continue
         l_sub = l[:-1]
-        cohesions[l] = pow(count/L[l[0]], 1/(n-1))        
+        cohesions[l] = pow(count/L[l[0]], 1/(n-1))
+        # Droprate
+        if n < 3:
+            continue
         droprate = count / L[l_sub]
         droprate_scores[l_sub] = max(droprate_scores.get(l_sub, 0), droprate)
-        if (l_sub in word_scores) and ((count / L[l_sub]) < expansion_threshold):
-            word_scores.pop(l_sub)
-        word_scores[l] = count
     droprate_scores = {word:1-score for word, score in droprate_scores.items() if len(word) > 1}
-    word_scores = {word:cohesions.get(word,0) * droprate_scores.get(word,1.0) for word in word_scores}
-    word_scores = {word:score for word,score in word_scores.items() if score > 0}
-    return cohesions, droprate_scores, word_scores
+    return cohesions, droprate_scores
 
-def _word_extraction(corpus, subword_max_length, word_scores):
+def _word_extraction(corpus, subword_max_length, droprate_scores, minimum_droprate_score):
     def tokenize(word):
-        words = [(word[:e], word_scores.get(word[:e], 0)) for e in range(2, min(subword_max_length, len(word))+1)]
-        if not words:
-            return ''
-        best = sorted(words, key=lambda x:-x[1])[0]
-        if best[1] > 0:
-            return best[0]
-        return ''
+        words = [(word[:e], droprate_scores.get(word[:e], 0)) for e in range(2, min(subword_max_length, len(word))+1)]
+        words = [entry[0] for entry in words if entry[1] >= minimum_droprate_score]
+        return words
     
     df = {}
     for n_doc, doc in enumerate(corpus):
-        words = [tokenize(word) for word in doc.split() if len(word) > 1]
-        words = [word for word in words if word]
+        words = [word for token in doc.split() for word in tokenize(token) if len(word) > 1]
+        words = {word for word in words if word}
         for word in words:
             df[word] = df.get(word, 0) + 1            
         if n_doc % 1000 == 999:
@@ -147,7 +142,7 @@ def main():
     parser.add_argument('--soynlp_path', type=str, default='./', help='soynlp package path')
     parser.add_argument('--min_frequency', type=int, default=100, help='minimum frequency for universial vocabulary construction')
     parser.add_argument('--subword_max_length', type=int, default=8, help='maximum length of left-side subsection (subword)')
-    parser.add_argument('--expansion_threshold', type=float, default=0.95, help='minimum #(w[:-1]) / #(w)')
+    parser.add_argument('--minimum_droprate_score', type=float, default=0.3, help='minimum #(w[:-1]) / #(w)')
     
     args = parser.parse_args()
     
@@ -156,7 +151,7 @@ def main():
     soynlp_path = args.soynlp_path
     min_frequency = args.min_frequency
     subword_max_length = args.subword_max_length
-    expansion_threshold = args.expansion_threshold
+    minimum_droprate_score = args.minimum_droprate_score
 
 #"""
 #"""
@@ -165,8 +160,12 @@ def main():
         os.makedirs(model_directory)
         print('created directory = {}'.format(model_directory))
     
+    print('{} corpus exist'.format(len(corpus_fnames)))
+    for corpus_fname in corpus_fnames:
+        print(corpus_fname)
+    
     print('begin parsing subwords')
-    parse_subwords(corpus_fnames, model_directory, soynlp_path, min_frequency, subword_max_length, expansion_threshold)
+    parse_subwords(corpus_fnames, model_directory, soynlp_path, min_frequency, subword_max_length, minimum_droprate_score)
     
     print('making universial vocabulary')
     universial_subwords = make_universial_vocabulary(corpus_fnames, model_directory, min_frequency)
