0ada163044d0f86138a742672d984663b3668449
Dongyoung <dongyoung.min@dm.snu.ac.kr>
2017-11-23 20:29:09 +0900

    Add functions for co-occurrence matrices

--- subword_df_handler_and_cooccurrence_calculator.py
+++ subword_df_handler_and_cooccurrence_calculator.py
@@ -99,6 +99,9 @@ def calculate_cooccurrence_document_frequency_ratio(category_sensitive_words_lis
     from collections import defaultdict
     
     for c, category_sensitive_words in enumerate(category_sensitive_words_list):
+        if len(category_sensitive_words) == 0:
+            print('category = {}, There are no category sensitive words here')
+            continue
         wc_occurrence = defaultdict(int) 
         cooccurrence = defaultdict(lambda: defaultdict(int))
         corpus_fname = '{}/{}.txt'.format(corpus_directory, c)
