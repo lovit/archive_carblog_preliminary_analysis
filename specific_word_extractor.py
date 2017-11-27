2196e14dfe1c3df311c17ff3d290dcd1a2cf6c36
lovit <soy.lovit@gmail.com>
2017-11-27 16:59:15 +0900

    Complete calculating cooccurrance (w_sensitive, w_positive)

--- specific_word_extractor.py
+++ specific_word_extractor.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
 import datetime
-import os
 import time
+import os
+from scipy.sparse import csr_matrix
+from scipy.io import mmwrite
 
 # Step 2. Extracting sensitive words for each category
 def extract_category_sensitive_words(pos_handler,
@@ -27,33 +29,73 @@ def extract_category_sensitive_words(pos_handler,
 def does_cooccurred(w_sens, w_pos, doc):
     return (w_sens in doc) and (w_pos in doc)
 
-def _calculate_cooccurrence_df(D_sensitive, D_positive, corpus_fname):
-    cooccurrence = defaultdict(lambda: defaultdict(int))
+def _calculate_cooccurrence_df(D_sensitive, D_positive, corpus_fname, debug=False):
+    D_positive = {' '+word.strip() for word in D_positive}
+    D_sensitive = {' '+word.strip() for word in D_sensitive}
+    cooccurrence_dd = defaultdict(lambda: defaultdict(int))
+    df_sensitive = defaultdict(int)
+    
     with open(corpus_fname, encoding='utf-8') as f:
         for i_doc, doc in enumerate(f):
             doc = ' ' + doc
             for i_ws, w_sens in enumerate(D_sensitive):
+                if debug and i_ws >= 199: break
+                if w_sens in doc:
+                    df_sensitive[w_sens] += 1
+                else:
+                    continue
                 for w_pos in D_positive:
                     if not does_cooccurred(w_sens, w_pos, doc):
                         continue
-                    cooccurrence[w_sens][w_pos] += 1
+                    cooccurrence_dd[w_sens][w_pos] += 1
+            if debug and i_doc >= 500: break
             if i_doc % 10 == 9:
                 print('\r  - calculating cooccurrence ... {} docs'.format(i_doc+1), flush=True, end='')
-    return {w_sens:dict(cooc_dict) for w_sens, cooc_dict in cooccurrence.items()}
-
-def calculate_cooccurrence_df(sensitive_words_by_category, D_positive, corpus_directory):
-    D_positive = {' '+word for word in D_positive}
-    word_by_category = []
     
-    for c, D_sensitive in enumerate(sensitive_words_by_category):
-        D_sensitive = {' '+word for word in D_sensitive}
-        corpus_fname = '{}/{}.txt'.format(corpus_directory, c)
+    cooccurrence_dd_normed = {}
+    for w_sens, cooccurrance_dict in cooccurrence_dd.items():
+        df_w_sens = df_sensitive[w_sens]
+        cooccurrence_dd_normed[w_sens.strip()] = {w_pos.strip():cooc/df_w_sens for w_pos, cooc in cooccurrance_dict.items()}
+    return cooccurrence_dd_normed, df_sensitive
 
+def _save_dictdict_as_sparsematrix(dd, subword2index, fname):
+    n_vocab = len(subword2index)
+    row_ind = []
+    col_ind = []
+    data = []
+    for w_sens, positive_word_counter in dd.items():
+        i = subword2index.get(w_sens, -1)
+        if i == -1:
+            continue
+        for positive_word, cooccur in positive_word_counter.items():
+            j = subword2index.get(positive_word, -1)
+            if j == -1:
+                continue
+            row_ind.append(i)
+            col_ind.append(j)
+            data.append(cooccur)
+    x = csr_matrix((data, (row_ind, col_ind)), shape=(n_vocab, n_vocab))
+    mmwrite(fname, x)
+
+def _save_df_as_list(df, subword2index, fname):
+    with open(fname, 'w', encoding='utf-8') as f:
+        for subword, _ in sorted(subword2index.items(), key=lambda x:x[1]):
+            f.write('{}\n'.format(df[subword]))
+
+def calculate_cooccurrence(D_sensitive_by_category, D_positive, subword2index, corpus_directory, model_directory, debug=False):
+    for c, D_sensitive in enumerate(D_sensitive_by_category):
+        if debug and c >= 3: 
+            break
+    
         process_time = time.time()
-        word_by_category.append(_calculate_cooccurrence_df(D_sensitive, D_positive, corpus_fname))
+        corpus_fname = '{}/{}.txt'.format(corpus_directory, c)
+        cooccurrence_dd_norm, df_sensitive = _calculate_cooccurrence_df(D_sensitive, D_positive, corpus_fname, debug)
         process_time = time.time() - process_time
         process_time = str(datetime.timedelta(seconds=process_time))
-
-        print('\rcalculating cooccurrence in {} / {} corpus ({})'.format(c, len(sensitive_words_by_category), process_time), flush=True)
-    return word_by_category
-
+        print('\rcalculating cooccurrence in {} / {} corpus ({})'.format(c, len(D_sensitive_by_category), process_time), flush=True)
+        if not cooccurrence_dd_norm:
+            continue
+        cooc_fname = '{}/cooccurrance_c{}.mtx'.format(model_directory, c)
+        df_fname = '{}/df_c{}.csv'.format(model_directory, c)
+        _save_dictdict_as_sparsematrix(cooccurrence_dd_norm, subword2index, cooc_fname)
+        _save_df_as_list(df_sensitive, subword2index, df_fname)
