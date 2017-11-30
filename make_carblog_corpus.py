c89cfb2876a443bc136fb33c89220eafd525715e
lovit <soy.lovit@gmail.com>
2017-11-30 21:31:24 +0900

    Commit carblog corpus maker

--- make_carblog_corpus.py
+++ make_carblog_corpus.py
@@ -1,4 +1,5 @@
 from glob import glob
+import argparse
 import json
 
 def parse(json_object):
@@ -23,29 +24,31 @@ def main():
     
     args = parser.parse_args()
     corpus_directory = args.corpus_directory
-    month_directories = glob('{}/*/*/*'.format(args.json_directory)) # root/category/year/month
+    car_directories = glob('{}/*'.format(args.json_directory)) # car, year, json
     
     import os
     if not os.path.exists(corpus_directory):
         os.makedirs(corpus_directory)
         print('created directory {}'.format(corpus_directory))
     
-    for n_corpus, month_directory in enumerate(month_directories):
-        corpus_name = '-'.join(month_directory.split('/')[-3:])
+    for i_corpus, car_root in enumerate(sorted(car_directories)):
+        corpus_name = car_root.split('/')[-1]
         corpus_fname = '{}/{}.txt'.format(corpus_directory, corpus_name)
         index_fname = '{}/{}.index'.format(corpus_directory, corpus_name)
         with open(corpus_fname, 'w', encoding='utf-8') as fc:
             with open(index_fname, 'w', encoding='utf-8') as fi:
-                json_fnames = glob('{}/*/*.json'.format(month_directory)) # month/date/json
+                json_fnames = glob('{}/*/*.json'.format(car_root))
                 for i, json_fname in enumerate(json_fnames):
                     with open(json_fname, encoding='utf-8') as f:
                         json_object = json.load(f)
+                    if not json_object or type(json_object) != dict:
+                        continue
                     corpus_content, index_content = parse(json_object)
                     fc.write('{}\n'.format(corpus_content))
                     fi.write('{}\n'.format(index_content))
                     if i % 200 == 199:
-                        print('\rparsing ... directory = {}/{}, json = {}/{}{}'.format(n_corpus+1, len(monthly_directories), i+1, len(json_fnames), ' '*20), flush=True, end='')
-        print('\rparsing done. directory = {}/{} {}'.format(n_corpus+1, len(monthly_directories), ' '*20))
+                        print('\rparsing ... directory = {}/{}, json = {}/{}{}'.format(i_corpus+1, len(car_directories), i+1, len(json_fnames), ' '*20), flush=True, end='')
+        print('\rparsing done. directory = {}/{}, {} complated {}'.format(i_corpus+1, len(car_directories), i+1, ' '*20))
 
 if __name__ == "__main__":
     main()
